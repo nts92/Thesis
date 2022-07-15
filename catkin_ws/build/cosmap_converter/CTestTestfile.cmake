16da0faa837dbe15ec99d131840ef0ea7e4c2cf0
nts92 <94467970+nts92@users.noreply.github.com>
2022-07-15 14:36:50 +0700

    Add files via upload

---
+++ catkin_ws/build/cosmap_converter/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/nts/catkin_ws/src/costmap_converter
+# Build directory: /home/nts/catkin_ws/build/costmap_converter
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(_ctest_costmap_converter_rostest_test_costmap_polygons.test "/home/nts/catkin_ws/build/catkin_generated/env_cached.sh" "/usr/bin/python3" "/opt/ros/noetic/share/catkin/cmake/test/run_tests.py" "/home/nts/catkin_ws/build/test_results/costmap_converter/rostest-test_costmap_polygons.xml" "--return-code" "/usr/bin/python3 /opt/ros/noetic/share/rostest/cmake/../../../bin/rostest --pkgdir=/home/nts/catkin_ws/src/costmap_converter --package=costmap_converter --results-filename test_costmap_polygons.xml --results-base-dir \"/home/nts/catkin_ws/build/test_results\" /home/nts/catkin_ws/src/costmap_converter/test/costmap_polygons.test ")
+set_tests_properties(_ctest_costmap_converter_rostest_test_costmap_polygons.test PROPERTIES  _BACKTRACE_TRIPLES "/opt/ros/noetic/share/catkin/cmake/test/tests.cmake;160;add_test;/opt/ros/noetic/share/rostest/cmake/rostest-extras.cmake;52;catkin_run_tests_target;/opt/ros/noetic/share/rostest/cmake/rostest-extras.cmake;80;add_rostest;/opt/ros/noetic/share/rostest/cmake/rostest-extras.cmake;100;_add_rostest_google_test;/home/nts/catkin_ws/src/costmap_converter/CMakeLists.txt;231;add_rostest_gtest;/home/nts/catkin_ws/src/costmap_converter/CMakeLists.txt;0;")
