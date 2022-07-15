9859f97a52091246728860dd979cf0d934dc0065
nts92 <94467970+nts92@users.noreply.github.com>
2022-07-15 14:40:44 +0700

    Add files via upload

---
+++ catkin_ws/build/data/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubun/catkin_ws/src/data
+# Build directory: /home/ubun/catkin_ws/build/data
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws/build/data_colect/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nts/catkin_ws/src/data_colect
+# Build directory: /home/nts/catkin_ws/build/data_colect
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
